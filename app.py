2af15889676d77c2209e725d9a5023cc3b7da96e
Pablo Guinea <48864366+pabloguineab@users.noreply.github.com>
2023-05-07 13:01:51 -0700

    Update app.py

--- app.py
+++ app.py
@@ -11,54 +11,59 @@ os.environ['OPENAI_API_KEY'] = st.secrets["apikey"]
 
 # App framework
 st.title('🦜🔗 Final undergraduate Project Generator')
-prompt = st.text_input('Plug in your prompt here') 
 
-# Prompt templates
-title_template = PromptTemplate(
-    input_variables=['topic'], 
-    template='write me a project degree title about {topic}'
-)
-index_template = PromptTemplate(
-    input_variables=['topic'], 
-    template='write me an extended index project degree about {topic}'
-)
+# User inputs
+prompt_title = st.text_input('Enter the project title:')
+prompt_index = st.text_input('Enter the project index:')
 
-script_template = PromptTemplate(
-    input_variables=['title', 'index', 'wikipedia_research'], 
-    template='write me an introduction section for my project based on this title TITLE: {title} and this index: {index} while leveraging this wikipedia research: {wikipedia_research}'
-)
-
-# Memory 
-title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
-index_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
-script_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
-
-
-# Llms
-llm = OpenAI(temperature=0.9) 
-title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=title_memory)
-index_chain = LLMChain(llm=llm, prompt=index_template, verbose=True, output_key='index', memory=index_memory)
-script_chain = LLMChain(
-    llm=llm,
-    prompt=script_template,
-    verbose=True,
-    output_key='script',
-    memory=script_memory
-)
-
-wiki = WikipediaAPIWrapper()
-
-# Show stuff to the screen if there's a prompt
-if prompt: 
-    title = title_chain.run(topic=prompt)
-    index = index_chain.run(topic=prompt)
+# Check if both inputs are provided
+if prompt_title and prompt_index:
+    # Prompt templates
+    title_template = PromptTemplate(
+        input_variables=['topic'], 
+        template='write me a project degree title about {topic}'
+    )
+    index_template = PromptTemplate(
+        input_variables=['topic'], 
+        template='write me an extended index project degree about {topic}'
+    )
+    script_template = PromptTemplate(
+        input_variables=['title', 'index', 'wikipedia_research'], 
+        template='write me an introduction section for my project based on this title TITLE: {title} and this index: {index} while leveraging this wikipedia research: {wikipedia_research}'
+    )
+    
+    # Memory 
+    title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
+    index_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
+    script_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
+    
+    # Llms
+    llm = OpenAI(temperature=0.9) 
+    title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=title_memory)
+    index_chain = LLMChain(llm=llm, prompt=index_template, verbose=True, output_key='index', memory=index_memory)
+    script_chain = LLMChain(
+        llm=llm,
+        prompt=script_template,
+        verbose=True,
+        output_key='script',
+        memory=script_memory
+    )
+    
+    wiki = WikipediaAPIWrapper()
+    
+    # Generate project based on user inputs
+    title = title_chain.run(topic=prompt_title)
+    index = index_chain.run(topic=prompt_index)
     wiki_research = wiki.run(prompt) 
     script = script_chain.run(title=title, index=index, wikipedia_research=wiki_research)
-
-    st.write(title) 
-    st.write(index)
-    st.write(script) 
-
+    
+    # Display project details
+    st.write("Generated Project:")
+    st.write("Title:", title)
+    st.write("Index:", index)
+    st.write("Script:", script)
+    
+    # Display history
     with st.expander('Title History'): 
         st.info(title_memory.buffer)
    
