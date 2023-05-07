fd7a96a7178870fddbfe8ab2a74b7fa982406b84
Pablo Guinea <48864366+pabloguineab@users.noreply.github.com>
2023-05-07 13:13:51 -0700

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ if prompt_title and prompt_index:
     # Generate project based on user inputs
     title = title_chain.run(topic=prompt_title)
     index = index_chain.run(topic=prompt_index)
-    wiki_research = wiki.run(prompt)  
+    wiki_research = wiki.run(prompt_title)  
     script = script_chain.run(title=title, index=index, wikipedia_research=wiki_research)
     
     # Display project details
