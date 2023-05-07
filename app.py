de2a6e27b6e51b8365364108aacd0d3ba76eebdc
Pablo Guinea <48864366+pabloguineab@users.noreply.github.com>
2023-05-07 11:29:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from langchain.chains import LLMChain, SequentialChain
 from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper 
 
-os.environ['OPENAI_API_KEY'] = apikey
+os.environ['OPENAI_API_KEY'] =  st.secrets["apikey"]
 
 # App framework
 st.title('🦜🔗 YouTube GPT Creator')
