baa992635f0407f237a88c25f35c9da3ec8e3ba2
Pablo Guinea <48864366+pabloguineab@users.noreply.github.com>
2023-05-07 14:06:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import streamlit as st 
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain 
 from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper 
 
@@ -14,23 +14,18 @@ st.title('🦜🔗 Final undergraduate Project Generator')
 
 # User inputs
 prompt_title = st.text_input('Enter the project title:')
-num_sections = st.number_input('Enter the number of sections in the index:', min_value=1, value=3, step=1)
-
-sections = []
-for i in range(num_sections):
-    section = st.text_input(f'Enter section {i+1} of the index:')
-    sections.append(section)
+prompt_index = st.text_input('Enter the project index:')
 
 # Check if both inputs are provided
-if prompt_title and sections:
+if prompt_title and prompt_index:
     # Prompt templates
     title_template = PromptTemplate(
         input_variables=['topic'], 
         template='write me a project degree title about {topic}'
     )
     index_template = PromptTemplate(
-        input_variables=['sections'], 
-        template='write me an extended index project degree with the following sections: {sections}'
+        input_variables=['topic'], 
+        template='write me an extended index project degree about {topic}'
     )
     script_template = PromptTemplate(
         input_variables=['title', 'index', 'wikipedia_research'], 
@@ -39,7 +34,7 @@ if prompt_title and sections:
     
     # Memory 
     title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
-    index_memory = ConversationBufferMemory(input_key='sections', memory_key='chat_history')
+    index_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
     script_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
     
     # Llms
@@ -58,7 +53,7 @@ if prompt_title and sections:
     
     # Generate project based on user inputs
     title = title_chain.run(topic=prompt_title)
-    index = index_chain.run(sections=[str(section) if section else "" for section in sections])
+    index = index_chain.run(topic=prompt_index)
     wiki_research = wiki.run(prompt_title)  
     script = script_chain.run(title=title, index=index, wikipedia_research=wiki_research)
     
