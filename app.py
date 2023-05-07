d5e8c51a0c15ec05f7a1a7e23eb984a2f135e798
Pablo Guinea <48864366+pabloguineab@users.noreply.github.com>
2023-05-07 13:52:23 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import streamlit as st 
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SequentialChain 
+from langchain.chains import LLMChain
 from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper 
 
